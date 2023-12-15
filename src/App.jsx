d1dee43e5612d230798f11bbf2b179ba97d2b8f8
Marina Perin <maryfrancisco@live.com>
2023-12-15 15:31:08 +0100

    feat | boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.scss'
+const apiKey = import.meta.env.VITE_API_KEY
+
+function App() {
+
+  
+  
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
